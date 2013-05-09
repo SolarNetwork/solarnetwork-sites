7af4eff75751d973ff92c43a6db116cd2cfd66b4
Matt Magoffin <git+matt@msqr.us>
2013-05-09 13:50:29 +1200

    Tweak to support POST.

--- nz.co.mypowercompany.console.web/web/js/sn.js
+++ nz.co.mypowercompany.console.web/web/js/sn.js
@@ -23,7 +23,8 @@ var SNAPI = {};
  */
 SNAPI.generateAuthorizationHeaderValue = function(params) {
 	var msg = 
-		(params.method === undefined ? 'GET' : params.method.toUpperCase()) + '\n\n\n'
+		(params.method === undefined ? 'GET' : params.method.toUpperCase()) + '\n\n'
+		+(params.data === undefined ? '' : 'application/x-www-form-urlencoded; charset=UTF-8') + '\n'
 		+params.date +'\n'
 		+params.path;
 	var hash = CryptoJS.HmacSHA1(msg, params.secret);
@@ -55,7 +56,7 @@ SNAPI.parseURLQueryTerms = function(search) {
 		for ( i = 0, len = pairs.length; i < len; i++ ) {
 			pair = pairs[i].split('=', 2);
 			if ( pair.length === 2 ) {
-				params[pair[0]] = pair[1];
+				params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
 			}
 		}
 	}
@@ -72,13 +73,13 @@ SNAPI.parseURLQueryTerms = function(search) {
  * @return {String} path the canonicalized path value to use in the SolarNetworkWS 
  *                       authorization header value
  */
-SNAPI.authURLPath = function(url) {
+SNAPI.authURLPath = function(url, data) {
 	var a = document.createElement('a');
 	a.href = url;
 	var path = a.pathname;
 	
 	// handle query params, which must be sorted
-	var params = SNAPI.parseURLQueryTerms(a.search);
+	var params = SNAPI.parseURLQueryTerms(data === undefined ? a.search : data);
 	var sortedKeys = [], key = undefined;
 	var i, len;
 	var first = true;
@@ -115,19 +116,20 @@ SNAPI.authURLPath = function(url) {
  * @param {String} url the web service URL to invoke
  * @param {Function} callback the function to call on success
  */
-SNAPI.requestJSON = function(url, callback, method) {
+SNAPI.requestJSON = function(url, method, data) {
 	method = (method === undefined ? 'GET' : method);
-	$.ajax({
+	var ajax = $.ajax({
 		type: method,
 		url: url,
 		dataType: 'json',
+		data: data,
 		beforeSend: function(xhr) {
 			// get a date, which we must include as a header as well as include in the 
 			// generated authorization hash
-			var date = new Date().toUTCString();
+			var date = new Date().toUTCString();		
 			
 			// construct our canonicalized path value from our URL
-			var path = SNAPI.authURLPath(url);
+			var path = SNAPI.authURLPath(url, data);
 			
 			// generate the authorization hash value now (cryptographically signing our request)
 			var auth = SNAPI.generateAuthorizationHeaderValue({
@@ -135,14 +137,19 @@ SNAPI.requestJSON = function(url, callback, method) {
 				date: date,
 				path: path,
 				token: SNAPI.ajaxCredentials.token,
-				secret: SNAPI.ajaxCredentials.secret
+				secret: SNAPI.ajaxCredentials.secret,
+				data: data
 			});
 			
 			// set the headers on our request
 			xhr.setRequestHeader('X-SN-Date', date);
 			xhr.setRequestHeader('Authorization', 'SolarNetworkWS ' +auth);
 		}
-	}).done(callback).fail(function(xhr, status, reason) {
+	});
+	return ajax;
+	/*
+	.done(callback).fail(function(xhr, status, reason) {
 		alert(reason + ': ' +status +' (' +xhr.status +')');
 	});
+	*/
 };
