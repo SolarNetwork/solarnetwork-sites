ea634b90e79a253ef1495343bd774b60e13d1e6d
Matt Magoffin <git+matt@msqr.us>
2014-05-21 20:46:21 +1200

    Work on defining stable source ID names and colors.

--- charts/energy-io/energy-io.js
+++ charts/energy-io/energy-io.js
@@ -11,13 +11,54 @@ sn.config.host = 'data.solarnetwork.net';
 sn.runtime.globalCounter = sn.counter();
 sn.runtime.excludeSources = new sn.Configuration();
 
-function setup(repInterval, sourceList) {
+function setup(repInterval, sourceMap) {
 	var endDate = repInterval.eDate;
 	var energyAreaChart = undefined;
 	
-	// create static mapping of source -> color, so consistent across charts
-	sn.runtime.colorData = sn.colorMap(sn.colors.steelblue, sourceList);
-	
+	// create static mapping of raw sources to potentially alternate names, to avoid name collisions;
+	// sourceMap is like {Consumption : [Main], Power : [Main]}, turn into
+	// {Consumption : { Main : 'Consumption/Main' }, Power : { Main : 'Power/Main' } }
+	// Also, create static mapping of source -> color, so consistent across charts.
+	var chartSourceMap = {};
+	var dataType = undefined;
+	var sourceList = [];
+	var colorGroup = undefined;
+	var sourceColors = [];
+	var typeSourceList = undefined;
+	var colorGroupIndex;
+	for ( dataType in sourceMap ) {
+		chartSourceMap[dataType] = {};
+		typeSourceList = [];
+		sourceMap[dataType].forEach(function(el) {
+			var mappedSource;
+			if ( el === '' || el === 'Main' ) {
+				mappedSource = dataType;
+			} else {
+				mappedSource = dataType +'/' +el;
+			}
+			chartSourceMap[dataType][el] = mappedSource;
+			typeSourceList.push(mappedSource);
+			sourceList.push(mappedSource);
+		});
+		if ( dataType === sn.env.dataTypes[0] ) {
+			// positive, make green
+			colorGroup = colorbrewer.Greens;
+		} else {
+			colorGroup = colorbrewer.Blues;
+		}
+		if ( typeSourceList.length < 3 ) {
+			colorGroupIndex = 3;
+		} else if ( colorGroup[typeSourceList.length] === undefined ) {
+			colorGroupIndex = 9;
+		} else {
+			colorGroupIndex = typeSourceList.length;
+		}
+		sourceColors = sourceColors.concat(colorGroup[colorGroupIndex].slice(-typeSourceList.length).reverse());
+	}
+	sn.runtime.chartSourceMap = chartSourceMap;
+
+	sn.runtime.colorData = sn.colorMap(sourceColors, sourceList);
+
 	// create copy of color data for reverse ordering so labels vertically match chart layers
 	sn.colorDataLegendTable('#source-labels', sn.runtime.colorData.slice().reverse(), legendClickHandler, function(s) {
 		if ( sn.env.linkOld === 'true' ) {
@@ -52,15 +93,51 @@ function setup(repInterval, sourceList) {
 			height: 400,
 			padding: [10, 0, 20, 30] // gives room to axis
 		});
+		var q = queue();
+		sn.env.dataTypes.forEach(function(e) {
+			q.defer(d3.json, sn.runtime.urlHelper.dateTimeQuery(e, wRange[0], wRange[1], sn.env.minutePrecision));
+		});
+		q.awaitAll(function(error, results) {
+			if ( error ) {
+				sn.log('Error requesting data: ' +error);
+				return;
+			}
+			var combinedData = [];
+			var i, iMax, j, jMax, json, datum, invert, mappedSourceId;
+			for ( i = 0, iMax = results.length; i < iMax; i++ ) {
+				json = results[i];
+				if ( json.success !== true || Array.isArray(json.data) !== true ) {
+					sn.log('No data available for node {0} data type {1}', sn.runtime.urlHelper.nodeId(), sn.env.dataTypes[i]);
+					return;
+				}
+				invert = (i > 0 && sn.env.wiggle !== 'true');
+				for ( j = 0, jMax = json.data.length; j < jMax; j++ ) {
+					datum = json.data[j];
+					if ( invert && datum.watts !== undefined ) {
+						datum.watts *= -100; // FIXME: this should be -1
+						//datum.watts = -datum.watts;
+					}
+					mappedSourceId = sn.runtime.chartSourceMap[sn.env.dataTypes[i]][datum.sourceId];
+					if ( mappedSourceId !== undefined ) {
+						datum.sourceId = mappedSourceId;
+					}
+				}
+				combinedData = combinedData.concat(json.data);
+			}
+			energyAreaChart.load(combinedData);
+			adjustChartDisplayUnits('.watt-chart', 'W', energyAreaChart.yScale());
+		});
+		/*
 		d3.json(sn.runtime.urlHelper.dateTimeQuery(sn.env.dataType, wRange[0], wRange[1], sn.env.minutePrecision), function(json) {
 			energyAreaChart.load(json.data);
 			adjustChartDisplayUnits('.watt-chart', 'W', energyAreaChart.yScale());
 		});
+		*/
 	}
 	
 	wattChartSetup(endDate);
 	setInterval(function() {
-		d3.json(sn.runtime.urlHelper.reportableInterval([sn.env.dataType]), function(error, json) {
+		d3.json(sn.runtime.urlHelper.reportableInterval(sn.env.dataTypes), function(error, json) {
 			if ( json.data === undefined || json.data.endDateMillis === undefined ) {
 				sn.log('No data available for node {0}: {1}', sn.runtime.urlHelper.nodeId(), (error ? error : 'unknown reason'));
 				return;
@@ -81,65 +158,32 @@ function setup(repInterval, sourceList) {
 			}, sn.config.defaultTransitionMs * .8);
 		}
 	}
-
-	function updateReadings() {
-		d3.json(sn.runtime.urlHelper.mostRecentQuery(sn.env.dataType), function(json) {
-			if ( json.data === undefined ) {
-				sn.log('No data available for node {0}', sn.runtime.urlHelper.nodeId());
-				return;
-			}
-		});
-	}
-
-	// every minute update reading values
-	updateReadings();
-	setInterval(function() {
-		updateReadings();
-	}, 60 * 1000);
 }
 
 function onDocumentReady() {
 	sn.setDefaultEnv({
 		nodeId : 30,
-		dataType : 'Power',
 		minutePrecision : 10,
 		numHours : 24,
 		numDays : 7,
 		wiggle : 'false',
 		linkOld : 'false',
 		maxPowerKW : 3,
+		dataTypes: ['Power', 'Consumption'] // first is positive, second is negative
 	});
 	sn.config.wChartRefreshMs = sn.env.minutePrecision * 60 * 1000;
 	
 	// setup DOM based on environment
 	d3.select('#num-days').text(sn.env.numDays);
 	d3.select('#num-hours').text(sn.env.numHours);
-	d3.selectAll('.watt-chart .dataType').text((function() {
-		if ( sn.env.dataType === 'Consumption' ) {
-			return 'use';
-		}
-		return 'output';
-	})());
-	d3.selectAll('.watthour-chart .dataType').text((function() {
-		if ( sn.env.dataType === 'Consumption' ) {
-			return 'consumption';
-		}
-		return 'production';
-	})());
-	d3.selectAll('#readings .total-power .dataType').text((function() {
-		if ( sn.env.dataType === 'Consumption' ) {
-			return 'Use';
-		}
-		return 'Power';
-	})());
 	d3.selectAll('.node-id').text(sn.env.nodeId);
 	
 	// find our available data range, and then draw our charts!
 	function handleAvailableDataRange(event) {
-		setup(event.data.reportableInterval, event.data.availableSources);
+		setup(event.data.reportableInterval, event.data.availableSourcesMap);
 		document.removeEventListener('snAvailableDataRange', handleAvailableDataRange, false);
 	}
 	document.addEventListener('snAvailableDataRange', handleAvailableDataRange, false);
 	sn.runtime.urlHelper = sn.NodeUrlHelper(sn.env.nodeId);
-	sn.availableDataRange(sn.runtime.urlHelper, ['Consumption', 'Power']);
+	sn.availableDataRange(sn.runtime.urlHelper, sn.env.dataTypes);
 }
