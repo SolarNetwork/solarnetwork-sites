9340682f8fd718e60a1cdaa0f78a84e833c99f14
Matt Magoffin <git+matt@msqr.us>
2014-05-30 15:06:23 +1200

    Start work on general stacked area chart with wiggle support and different aggregate levels.

---
+++ charts/power-area/power-area.js
@@ -0,0 +1,200 @@
+/**
+ * @require d3 3.0
+ * @require queue 1.0
+ * @require solarnetwork-d3 0.0.3
+ * @require solarnetwork-d3-chart-power-io 1.0.0
+ */
+
+sn.config.debug = true;
+sn.config.host = 'data.solarnetwork.net';
+sn.runtime.excludeSources = new sn.Configuration();
+
+//adjust display units as needed (between W and kW, etc)
+function adjustChartDisplayUnits(chartKey, baseUnit, scale) {
+	var unit = (scale === 1000000 ? 'M' : scale === 1000 ? 'k' : '') + baseUnit;
+	d3.selectAll(chartKey +' .unit').text(unit);
+}
+
+//handle clicks on legend handler
+function legendClickHandler(d, i) {
+	sn.runtime.excludeSources.toggle(d.source);
+	if ( sn.runtime.powerAreaChart !== undefined ) {
+		// use a slight delay, otherwise transitions can be jittery
+		setTimeout(function() {
+			sn.runtime.powerAreaChart.regenerate();
+			adjustChartDisplayUnits('.watt-chart', 'W', sn.runtime.powerAreaChart.yScale());
+		}, sn.runtime.powerAreaChart.transitionMs() * 0.5);
+	}
+}
+
+// Watt stacked area chart
+function wattChartSetup(endDate, sourceMap) {
+	var precision = (sn.env.minutePrecision || 10);
+	var timeCount = (sn.env.numHours || 24);
+	var timeUnit = 'hour';
+	var end = d3.time.minute.utc.ceil(endDate);
+	end.setUTCMinutes((end.getUTCMinutes() + precision - (end.getUTCMinutes() % precision)), 0, 0);
+	var start = d3.time.hour.utc.offset(end, -timeCount);
+	
+	d3.select('.watt-chart .time-count').text(timeCount);
+	d3.select('.watt-chart .time-unit').text(timeUnit);
+	
+	var q = queue();
+	sn.env.dataTypes.forEach(function(e, i) {
+		var urlHelper = (i === 0 ? sn.runtime.consumptionUrlHelper : sn.runtime.urlHelper);
+		q.defer(d3.json, urlHelper.dateTimeQuery(e, start, end, sn.env.minutePrecision));
+	});
+	q.awaitAll(function(error, results) {
+		if ( error ) {
+			sn.log('Error requesting data: ' +error);
+			return;
+		}
+		var combinedData = [];
+		var i, iMax, j, jMax, json, datum, mappedSourceId;
+		for ( i = 0, iMax = results.length; i < iMax; i++ ) {
+			json = results[i];
+			if ( json.success !== true || Array.isArray(json.data) !== true ) {
+				sn.log('No data available for node {0} data type {1}', sn.runtime.urlHelper.nodeId(), sn.env.dataTypes[i]);
+				return;
+			}
+			for ( j = 0, jMax = json.data.length; j < jMax; j++ ) {
+				datum = json.data[j];
+				mappedSourceId = sn.runtime.sourceColorMap.displaySourceMap[sn.env.dataTypes[i]][datum.sourceId];
+				if ( mappedSourceId !== undefined ) {
+					datum.sourceId = mappedSourceId;
+				}
+			}
+			combinedData = combinedData.concat(json.data);
+		}
+		sn.runtime.powerAreaChart.load(combinedData);
+		sn.log("Power IO chart watt range: {0}", sn.runtime.powerAreaChart.yDomain());
+		sn.log("Power IO chart time range: {0}", sn.runtime.powerAreaChart.xDomain());
+		adjustChartDisplayUnits('.watt-chart', 'W', sn.runtime.powerAreaChart.yScale());
+	});
+}
+
+function setup(repInterval, sourceMap) {
+	sn.runtime.reportableEndDate = repInterval.eLocalDate;
+	sn.runtime.sourceMap = sourceMap;
+	sn.runtime.sourceColorMap = sn.sourceColorMapping(sourceMap);
+	
+	// we make use of sn.colorFn, so stash the required color map where expected
+	sn.runtime.colorData = sn.runtime.sourceColorMap.colorMap;
+
+	// set up form-based details
+	d3.select('#details .consumption').style('color', 
+			sn.runtime.sourceColorMap.colorMap[sn.runtime.sourceColorMap.displaySourceMap['Consumption'][sourceMap['Consumption'][0]]]);
+	d3.select('#details .generation').style('color', 
+			sn.runtime.sourceColorMap.colorMap[sn.runtime.sourceColorMap.displaySourceMap['Power'][sourceMap['Power'][0]]]);
+
+	// create copy of color data for reverse ordering so labels vertically match chart layers
+	sn.colorDataLegendTable('#source-labels', sn.runtime.sourceColorMap.colorMap.slice().reverse(), legendClickHandler, function(s) {
+		if ( sn.env.linkOld === 'true' ) {
+			s.html(function(d) {
+				return '<a href="' +sn.runtime.urlHelper.nodeDashboard(d) +'">' +d +'</a>';
+			});
+		} else {
+			s.text(Object);
+		}
+	});
+
+	wattChartSetup(sn.runtime.reportableEndDate, sn.runtime.sourceMap);
+}
+
+function urlHelperForAvailbleDataRange(e, i) {
+	if ( !arguments.length ) return sn.runtime.urlHelper;
+	return (i === 0 ? sn.runtime.consumptionUrlHelper : sn.runtime.urlHelper);
+}
+
+function setupUI() {
+	d3.selectAll('.node-id').text(sn.env.nodeId);
+
+	// update details form based on env
+	['nodeId', 'consumptionNodeId', 'numHours', 'numDays', 'numMonths', 'numYears'].forEach(function(e) {
+		d3.select('input[name='+e+']').property('value', sn.env[e]);
+	});
+	d3.select('input[name=wiggle]').attr('checked', function() {
+		return (sn.env.wiggle === 'true' ? 'checked' : null);
+	});
+
+	// update the chart details
+	d3.selectAll('#details input').on('change', function(e) {
+		var me = d3.select(this);
+		var propName = me.attr('name');
+		var getAvailable = false;
+		if ( this.type === 'checkbox' ) {
+			sn.env[propName] = me.property('checked');
+		} else {
+			sn.env[propName] = me.property('value');
+		}
+		if ( propName === 'consumptionNodeId' ) {
+			sn.runtime.consumptionUrlHelper = sn.nodeUrlHelper(sn.env[propName]);
+			getAvailable = true;
+		} else if ( propName === 'nodeId' ) {
+			sn.runtime.urlHelper = sn.nodeUrlHelper(sn.env[propName]);
+			getAvailable = true;
+		} else if ( propName === 'wiggle' ) {
+			sn.runtime.powerAreaParameters.value(propName, sn.env[propName]);
+			sn.runtime.powerAreaChart.regenerate();
+			return;
+		}
+		if ( getAvailable ) {
+			sn.availableDataRange(urlHelperForAvailbleDataRange, sn.env.dataTypes);
+		} else {
+			wattChartSetup(sn.runtime.reportableEndDate, sn.runtime.sourceMap);
+		}
+	});
+}
+
+function onDocumentReady() {
+	sn.setDefaultEnv({
+		nodeId : 111,
+		consumptionNodeId : 108,
+		minutePrecision : 10,
+		numHours : 24,
+		wiggle : 'true',
+		linkOld : 'false',
+		dataTypes: ['Consumption', 'Power']
+	});
+	
+	sn.runtime.wChartRefreshMs = sn.env.minutePrecision * 60 * 1000;
+
+	sn.runtime.powerAreaParameters = new sn.Configuration({
+		aggregate : 'Hour',
+		excludeSources : sn.runtime.excludeSources,
+		northernHemisphere : (sn.env.northernHemisphere === 'true' ? true : false),
+		wiggle : (sn.env.wiggle === 'true')
+	});
+	
+	sn.runtime.powerAreaChart = sn.chart.powerAreaChart('#watt-chart', sn.runtime.powerAreaParameters);
+	
+	setupUI();
+
+	// find our available data range, and then draw our charts!
+	function handleAvailableDataRange(event) {
+		setup(event.data.reportableInterval, event.data.availableSourcesMap);
+		
+		if ( sn.runtime.refreshTimer === undefined ) {
+			// refresh chart data on interval
+			sn.runtime.refreshTimer = setInterval(function() {
+				d3.json(sn.runtime.urlHelper.reportableInterval(sn.env.dataTypes), function(error, json) {
+					if ( json.data === undefined || json.data.endDateMillis === undefined ) {
+						sn.log('No data available for node {0}: {1}', sn.runtime.urlHelper.nodeId(), (error ? error : 'unknown reason'));
+						return;
+					}
+					if ( sn.runtime.powerAreaChart !== undefined ) {
+						var jsonEndDate = sn.dateTimeFormatLocal.parse(json.data.endDate);
+						if ( jsonEndDate.getTime() > sn.runtime.reportableEndDate.getTime() ) {
+							sn.runtime.reportableEndDate = jsonEndDate;
+							wattChartSetup(jsonEndDate, sn.runtime.sourceMap);
+						}
+					}
+				});
+			}, sn.runtime.wChartRefreshMs);
+		}
+	}
+	document.addEventListener('snAvailableDataRange', handleAvailableDataRange, false);
+	sn.runtime.urlHelper = sn.nodeUrlHelper(sn.env.nodeId);
+	sn.runtime.consumptionUrlHelper = sn.nodeUrlHelper(sn.env.consumptionNodeId);
+	sn.availableDataRange(urlHelperForAvailbleDataRange, sn.env.dataTypes);
+}
---
+++ charts/power-area/solarnetwork-d3-chart-power-area.js
@@ -0,0 +1,363 @@
+/**
+ * @require d3 3.0
+ * @require queue 1.0
+ * @require solarnetwork-d3 0.0.3
+ */
+
+if ( sn === undefined ) {
+	sn = { chart: {} };
+} else if ( sn.chart === undefined ) {
+	sn.chart = {};
+}
+
+/**
+ * @typedef sn.chart.powerAreaChartParameters
+ * @type {sn.Configuration}
+ * @property {number} [width=812] - desired width, in pixels, of the chart
+ * @property {number} [height=300] - desired height, in pixels, of the chart
+ * @property {number[]} [padding=[10, 0, 20, 30]] - padding to inset the chart by, in top, right, bottom, left order
+ * @property {number} [transitionMs=600] - transition time
+ * @property {sn.Configuration} excludeSources - the sources to exclude from the chart
+ */
+
+/**
+ * An power stacked area chart.
+ * 
+ * You can use the {@code excludeSources} parameter to dynamically alter which sources are visible
+ * in the chart. After changing the configuration call {@link sn.chart.powerAreaChart#regenerate()}
+ * to re-draw the chart.
+ * 
+ * Note that the global {@link sn.colorFn} function is used to map sources to colors, so that
+ * must be set up previously.
+ * 
+ * @class
+ * @param {string} containerSelector - the selector for the element to insert the chart into
+ * @param {sn.chart.powerAreaChartParameters} [chartConfig] - the chart parameters
+ * @returns {sn.chart.powerAreaChart}
+ */
+sn.chart.powerAreaChart = function(containerSelector, chartConfig) {
+	var that = {
+		version : "1.0.0"
+	};
+	var sources = [];
+	var config = (chartConfig || new sn.Configuration());
+	
+	// default to container's width, if we can
+	var containerWidth = sn.pixelWidth(containerSelector);
+	
+	var p = (config.padding || [10, 0, 20, 30]),
+		w = (config.width || containerWidth || 812) - p[1] - p[3],
+		h = (config.height || 300) - p[0] - p[2],
+    	x = d3.time.scale.utc().range([0, w]),
+		y = d3.scale.linear().range([h, 0]),
+		format = d3.time.format("%H");
+
+	var transitionMs = undefined;
+
+	// the d3 stack offset method, or function
+	var stackOffset = undefined;
+
+	var svgRoot = undefined,
+		svg = undefined,
+		svgTickGroupX = undefined;
+	
+	// our layer data, and generator function
+	var layerGenerator = undefined;
+	var layers = undefined;
+	var minY = 0;
+
+	function parseConfiguration() {
+		aggregateType = (config.aggregate === 'Month' ? 'Month' : config.aggregate === 'Day' ? 'Day' : 'Hour');
+		transitionMs = (config.transitionMs || 600);
+		vertRuleOpacity = (config.vertRuleOpacity || 0.05);
+		seasonColors = (config.seasonColors || ['#5c8726', '#e9a712', '#762123', '#80a3b7']);
+		northernHemisphere = (config.northernHemisphere === true ? true : false);
+		stackOffset = (config.wiggle === true ? 'wiggle' : 'zero');
+	}
+	
+	parseConfiguration();
+
+	svgRoot = d3.select(containerSelector).select('svg');
+	if ( svgRoot.empty() ) {
+		svgRoot = d3.select(containerSelector).append('svg:svg')
+			.attr('class', 'chart')
+			.attr("width", w + p[1] + p[3])
+			.attr("height", h + p[0] + p[2]);
+	} else {
+		svgRoot.selectAll('*').remove();
+	}
+
+	svg = svgRoot.append("g")
+		.attr('class', 'data')
+		.attr("transform", "translate(" + p[3] + "," + p[0] + ")");
+	
+	svgTickGroupX = svgRoot.append("g")
+		.attr("class", "ticks")
+		.attr("transform", "translate(" + p[3] +"," +(h + p[0] + p[2]) +")");
+
+	svgRoot.append("g")
+		.attr("class", "crisp rule")
+		.attr("transform", "translate(0," + p[0] + ")");
+
+	function strokeColorFn(d, i) { return d3.rgb(sn.colorFn(d,i)).darker(); }
+
+	var areaPathGenerator = d3.svg.area()
+		.interpolate("monotone")
+		.x(function(d) { return x(d.x); })
+		.y0(function(d) { return y(d.y0); })
+		.y1(function(d) { return y(d.y0 + d.y); });
+	
+	function computeDomainX() {
+		x.domain(layers.domainX);
+	}
+
+	function computeDomainY() {
+		y.domain([minY, layers.maxY]).nice();
+		computeUnitsY();
+	}
+	
+	// Set y-axis  unit label
+	// setup display units in kW if domain range > 1000
+	var displayFactor = 1;
+	var displayFormatter = d3.format(',d');
+	function computeUnitsY() {
+		var fmt;
+		var maxY = d3.max(y.domain() ,function(v) { return Math.abs(v); });
+		if ( maxY >= 100000 ) {
+			displayFactor = 1000000;
+			fmt = ',g';
+		} else if ( maxY >= 1000 ) {
+			displayFactor = 1000;
+			fmt = ',g';
+		} else {
+			displayFactor = 1;
+			fmt = ',d';
+		}
+		displayFormatter = d3.format(fmt);
+	}
+	
+	function displayFormat(d) {
+		return displayFormatter(d / displayFactor);
+	}
+
+	function setup(rawData) {
+		// turn filteredData object into proper array, sorted by date
+		sources = [];
+		var dataArray = sn.powerPerSourceArray(rawData, sources);
+		sn.log('Available area sources: {0}', sources);
+
+		// Transpose the data into watt layers by source, e.g.
+		// [ [{x:0,y:0},{x:1,y:1}...], ... ]
+		layerGenerator = sn.powerPerSourceStackedLayerGenerator(sources, 'watts')
+			.excludeSources(config.excludeSources)
+			.offset(stackOffset)
+			.data(dataArray);
+		layers = layerGenerator();
+
+		// Compute the x-domain (by date) and y-domain (by top).
+		computeDomainX();
+		computeDomainY();
+	}
+
+	function redraw() {	
+		// draw data areas
+		var area = svg.selectAll("path.area").data(layers);
+		
+		area.transition().duration(transitionMs).delay(200)
+				.attr("d", areaPathGenerator)
+				.style("fill", sn.colorFn);
+		
+		area.enter().append("path")
+				.attr("class", "area")
+				.style("fill", sn.colorFn)
+				.attr("d", areaPathGenerator);
+		
+		area.exit().remove();
+	}
+
+	function axisYTransform(d) {
+		// align to half-pixels, to 1px line is aligned to pixels and crisp
+		return "translate(0," + (Math.round(y(d) + 0.5) - 0.5) + ")"; 
+	};
+
+	function adjustAxisX() {
+		if ( d3.event && d3.event.transform ) {
+			d3.event.transform(x);
+		}
+		var numTicks = 12;
+		var fx = x.tickFormat(numTicks);
+		var ticks = x.ticks(numTicks);
+
+		// Generate x-ticks
+		var labels = svgTickGroupX.selectAll("text").data(ticks);
+		
+		labels.transition().duration(transitionMs)
+	  		.attr("x", x)
+	  		.text(fx);
+		
+		labels.enter().append("text")
+			.attr("dy", "-0.5em") // needed so descenders not cut off
+			.style("opacity", 1e-6)
+			.attr("x", x)
+		.transition().duration(transitionMs)
+				.style("opacity", 1)
+				.text(fx)
+				.each('end', function() {
+						// remove the opacity style
+						d3.select(this).style("opacity", null);
+					});
+		labels.exit().transition().duration(transitionMs)
+			.style("opacity", 1e-6)
+			.remove();
+	}
+	
+	function axisRuleClassY(d) {
+		return (d === 0 ? 'origin' : 'm');
+	}
+
+	function adjustAxisY() {
+		var axisLines = svgRoot.select("g.rule").selectAll("g").data(
+				that.wiggle() ? [] : y.ticks(5));
+		var axisLinesT = axisLines.transition().duration(transitionMs);
+		axisLinesT.attr("transform", axisYTransform)
+			.select("text")
+				.text(displayFormat);
+		axisLinesT.select("line")
+				.attr('class', axisRuleClassY);
+		
+	  	axisLines.exit().transition().duration(transitionMs)
+	  			.style("opacity", 1e-6)
+	  			.remove();
+	  			
+		var entered = axisLines.enter()
+				.append("g")
+				.style("opacity", 1e-6)
+	  			.attr("transform", axisYTransform);
+		entered.append("line")
+				.attr("x2", w + p[3])
+				.attr('x1', p[3])
+				.attr('class', axisRuleClassY);
+		entered.append("text")
+				.attr("x", p[3] - 10)
+				.text(displayFormat);
+		entered.transition().duration(transitionMs)
+				.style("opacity", 1)
+				.each('end', function() {
+					// remove the opacity style
+					d3.select(this).style("opacity", null);
+				});
+	}
+	
+	that.sources = sources;
+	
+	/**
+	 * Get the x-axis domain (minimum and maximum dates).
+	 * 
+	 * @return {number[]} an array with the minimum and maximum values used in the x-axis of the chart
+	 * @memberOf sn.chart.powerAreaChart
+	 */
+	that.xDomain = function() { return x.domain(); };
+
+	/**
+	 * Get the y-axis domain (minimum and maximum values).
+	 * 
+	 * @return {number[]} an array with the minimum and maximum values used in the y-axis of the chart
+	 * @memberOf sn.chart.powerAreaChart
+	 */
+	that.yDomain = function() { return y.domain(); };
+	
+	/**
+	 * Get the scaling factor the y-axis is using. By default this will return {@code 1}.
+	 * After calling the {@link #load()} method, however, the chart may decide to scale
+	 * the y-axis for clarity. You can call this method to find out the scaling factor the
+	 * chart ended up using.
+	 *  
+	 * @return the y-axis scale factor
+	 * @memberOf sn.chart.powerAreaChart
+	 */
+	that.yScale = function() { return displayFactor; };
+
+	/**
+	 * Load data for the chart. The data is expected to be in a form suitable for
+	 * passing to {@link sn.powerPerSourceArray}.
+	 * 
+	 * @param {Array} rawData - the raw chart data to load
+	 * @return this object
+	 * @memberOf sn.chart.powerAreaChart
+	 */
+	that.load = function(rawData) {
+		parseConfiguration();
+		setup(rawData);
+		adjustAxisX();
+		adjustAxisY();
+		redraw();
+		return that;
+	};
+	
+	/**
+	 * Regenerate the chart, using the current data. This can be called after disabling a
+	 * source 
+	 * 
+	 * @return this object
+	 * @memberOf sn.chart.powerAreaChart
+	 */
+	that.regenerate = function() {
+		if ( layerGenerator === undefined ) {
+			// did you call load() first?
+			return that;
+		}
+		parseConfiguration();
+		layerGenerator.offset(that.stackOffset());
+		layers = layerGenerator();
+		computeDomainY();
+		adjustAxisY();
+		redraw();
+		return that;
+	};
+	
+	/**
+	 * Get or set the animation transition time, in milliseconds.
+	 * 
+	 * @param {number} [value] the number of milliseconds to use
+	 * @return when used as a getter, the millisecond value, otherwise this object
+	 * @memberOf sn.chart.powerAreaChart
+	 */
+	that.transitionMs = function(value) {
+		if ( !arguments.length ) return transitionMs;
+		transitionMs = +value; // the + used to make sure we have a Number
+		return that;
+	};
+
+	/**
+	 * Get or set the d3 stack offset.
+	 * 
+	 * This can be any supported d3 stack offset, such as 'wiggle' or a custom function.
+	 * 
+	 * @param {string|function} [value] the stack offset to use
+	 * @return when used as a getter, the stack offset value, otherwise this object
+	 * @memberOf sn.chart.powerAreaChart
+	 */
+	that.stackOffset = function(value) {
+		if ( !arguments.length ) return stackOffset;
+		stackOffset = value;
+		return that;
+	};
+
+	/**
+	 * Get or set the "wiggle" stack offset method.
+	 * 
+	 * This is an alias for the {@link #stackOffset} function, specifically to set the {@code wiggle}
+	 * style offset if passed <em>true</em> or the {@code zero} offset if <em>false</em>.
+	 * 
+	 * @param {boolean} [value] <em>true</em> to use the {@code wiggle} offset, <em>false</em> to use {@code zero}
+	 * @return when used as a getter, <em>true</em> if {@code wiggle} is the current offset, <em>false</em> otherwise;
+	 *         when used as a setter, this object
+	 * @memberOf sn.chart.powerAreaChart
+	 */
+	that.wiggle = function(value) {
+		if ( !arguments.length ) return (stackOffset === 'wiggle');
+		return that.stackOffset(value === true ? 'wiggle' : 'zero');
+	};
+
+	return that;
+};
