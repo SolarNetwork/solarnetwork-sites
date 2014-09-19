0e1b0448c8043ab496215cd4710c4f97907f0af8
Matt Magoffin <git+matt@msqr.us>
2014-09-19 12:10:19 +1200

    Add utility to extract a Date from a Datum.

--- charts/sn-lib/solarnetwork-d3-datum.js
+++ charts/sn-lib/solarnetwork-d3-datum.js
@@ -530,4 +530,31 @@ sn.datum.multiLoader = function(loaders) {
 	return that;
 };
 
+/**
+ * Get a Date object for a datum. This function will return the first available date according
+ * to the first available property found according to these rules:
+ * 
+ * <ol>
+ * <li><code>date</code> - assumed to be a Date object already</li>
+ * <li><code>localDate</code> - a string in <b>yyyy-MM-dd</b> form, optionally with a String
+ *     <code>localTime</code> property for an associated time in <b>HH:mm</b> form.</li>
+ * <li><code>created</code> - a string in <b>yyyy-MM-dd HH:mm:ss.SSS'Z'</b> form.</li>
+ * </ul>
+ *
+ * @param {Object} d The datum to get the Date for.
+ * @returns {Date} The found Date, or <em>null</em> if not available
+ */
+sn.datum.datumDate = function(d) {
+	if ( d ) {
+		if ( d.date ) {
+			return d.date;
+		} else if ( d.localDate ) {
+			return sn.dateTimeFormat.parse(d.localDate +(d.localTime ? ' ' +d.localTime : ' 00:00'));
+		} else if ( d.created ) {
+			return sn.timestampFormat.parse(d.created);
+		}
+	}
+	return null;
+};
+
 }());
