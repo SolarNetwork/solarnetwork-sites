411aba6b3ce94fdb5f2f7ff96e5ebd05ffb1b17e
Matt Magoffin <git+matt@msqr.us>
2015-02-28 19:20:14 +1300

    Add sn.user.userUrlHelper class to support user-level API methods.

--- charts/sn-lib/solarnetwork-d3-user.js
+++ charts/sn-lib/solarnetwork-d3-user.js
@@ -6,9 +6,11 @@
 (function() {
 'use strict';
 
+var userUrlHelperFunctions;
+
 if ( sn.user === undefined ) {
 	/**
-	 * @namespace the SolarNetwork security namespace.
+	 * @namespace the SolarNetwork user namespace.
 	 */
 	sn.user = {
 		version : '1.0.0'
@@ -49,5 +51,110 @@ sn.datum.registerNodeUrlHelperFunction('queueInstructionURL', function(topic, pa
 	}
 	return url;
 });
+
+/**
+ * An active user-specific URL utility object. This object does not require
+ * any specific user ID to be configured, as all requests are assumed to apply
+ * to the active user credentials.
+ * 
+ * @class
+ * @constructor
+ * @param {Object} configuration The configuration options to use.
+ * @returns {sn.user.userUrlHelper}
+ */
+sn.user.userUrlHelper = function(configuration) {
+	var self = {
+		version : '1.0.0'
+	};
+	
+	var config = sn.util.copy(configuration, {
+		host : 'data.solarnetwork.net',
+		tls : true,
+		path : '/solaruser',
+		secureQuery : true
+	});
+	
+	/**
+	 * Get a URL for just the SolarNet host, without any path.
+	 *
+	 * @returns {String} the URL to the SolarNet host
+	 * @memberOf sn.datum.nodeUrlHelper
+	 */
+	function hostURL() {
+		return ('http' +(config.tls === true ? 's' : '') +'://' +config.host);
+	}
+	
+	/**
+	 * Get a URL for the SolarNet host and the base API path, e.g. <code>/solaruser/api/v1/sec</code>.
+	 *
+	 * @returns {String} the URL to the SolarUser base API path
+	 * @memberOf sn.user.userUrlHelper
+	 */
+	function baseURL() {
+		return (hostURL() +config.path +'/api/v1/' +(config.secureQuery === true ? 'sec' : 'pub'));
+	}
+	
+	/**
+	 * Get a description of this helper object.
+	 *
+	 * @return {String} The description of this object.
+	 * @memberOf sn.user.userUrlHelper
+	 */
+	function keyDescription() {
+		return 'user';
+	}
+	
+	/**
+	 * Generate a SolarUser {@code /nodes} URL.
+	 * 
+	 * @return {String} the URL to access the active user's nodes
+	 * @memberOf sn.user.userUrlHelper
+	 */
+	function viewNodesURL(sourceIds) {
+		var url = (baseURL() + '/nodes');
+		return url;
+	}
+	
+	// setup core properties
+	Object.defineProperties(self, {
+		keyDescription			: { value : keyDescription },
+		hostURL					: { value : hostURL },
+		baseURL					: { value : baseURL },
+		viewNodesURL 			: { value : viewNodesURL }
+	});
+	
+	// allow plug-ins to supply URL helper methods, as long as they don't override built-in ones
+	(function() {
+		if ( Array.isArray(userUrlHelperFunctions) ) {
+			userUrlHelperFunctions.forEach(function(helper) {
+				if ( self.hasOwnProperty(helper.name) === false ) {
+					Object.defineProperty(self, helper.name, { value : function() {
+						return helper.func.apply(self, arguments);
+					} });
+				}
+			});
+		}
+	}());
+
+	return self;
+};
+
+/**
+ * Register a custom function to generate URLs with {@link sn.user.userUrlHelper}.
+ * 
+ * @param {String} name The name to give the custom function. By convention the function
+ *                      names should end with 'URL'.
+ * @param {Function} func The function to add to sn.user.userUrlHelper instances.
+ */
+sn.user.registerUserUrlHelperFunction = function(name, func) {
+	if ( typeof func !== 'function' ) {
+		return;
+	}
+	if ( userUrlHelperFunctions === undefined ) {
+		userUrlHelperFunctions = [];
+	}
+	name = name.replace(/[^0-9a-zA-Z_]/, '');
+	userUrlHelperFunctions.push({name : name, func : func});
+};
 
 }());
